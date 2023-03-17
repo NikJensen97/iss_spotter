035e4e316d5588ce15f72844fb26e6369d35fda9
NikJensen97 <npjjensen@hotmail.com>
2023-03-16 18:34:32 -0700

    added flyovercoords

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
-
+const { fetchISSFlyOverTimes } = require('./iss');
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -19,3 +19,12 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Longitude and Latitude are:' , ip);
 });
 */
+const mycoords = { latitude: 49.1665898, longitude: -123.133569 };
+fetchISSFlyOverTimes(mycoords, (error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
