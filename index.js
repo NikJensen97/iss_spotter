116728861fa7bfbe98e9a0f69eb7c7281e05828d
NikJensen97 <npjjensen@hotmail.com>
2023-03-16 19:04:16 -0700

    only imports iss megafunction and runs it

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const { fetchMyIP } = require('./iss');
+/*const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
-fetchMyIP((error, ip) => {
+*/
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
+});
+
+/*fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -19,8 +41,8 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Longitude and Latitude are:' , ip);
 });
 */
-const mycoords = { latitude: 49.1665898, longitude: -123.133569 };
-fetchISSFlyOverTimes(mycoords, (error, ip) => {
+//const mycoords = { latitude: 49.1665898, longitude: -123.133569 };
+/*fetchISSFlyOverTimes(mycoords, (error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -28,3 +50,4 @@ fetchISSFlyOverTimes(mycoords, (error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+*/
