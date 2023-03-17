2a53205c953c8797867870a75bf6a61a8bf76241
NikJensen97 <npjjensen@hotmail.com>
2023-03-16 18:34:45 -0700

    added flyovercoords

--- iss.js
+++ iss.js
@@ -46,6 +46,21 @@ const fetchCoordsByIP = function(ip, callback) {
 
   });
 };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    console.log(data.response);
 
+  });
+};
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
