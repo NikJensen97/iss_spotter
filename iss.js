842f64b10584f67af0243acc563f6fd377885468
NikJensen97 <npjjensen@hotmail.com>
2023-03-16 18:04:25 -0700

    added fetchcoords function

---
+++ iss.js
@@ -0,0 +1,51 @@
+
+const request = require('request');
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    callback(error, data.ip);
+  });
+
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    if (!data.success) {
+      const message = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    let coords = {
+      'latitude': data.latitude,
+      'longitude': data.longitude
+    };
+    callback(error, coords);
+
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
